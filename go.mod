769399fb4a05587d79cfee69c7b4da875fe77584
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-12-20 17:58:31 +0700

    second_commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zflash123/app_say_hello
 
 go 1.19
+
+require github.com/zflash123/go_say_hello v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main(){
-	
+import(
+	"fmt"
+	go_say_hello "github.com/zflash123/go_say_hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
 }
